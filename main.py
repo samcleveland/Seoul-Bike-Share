06a4300308653cd802a977d7d122e15af494ddfa
samcleveland <52433135+samcleveland@users.noreply.github.com>
2022-01-31 17:31:18 -0600

    Clean up
    Cleaning up previous code & adding comments

--- main.py
+++ main.py
@@ -13,25 +13,28 @@ import pandas as pd
 from sklearn import linear_model, preprocessing
 import os
 from sklearn.model_selection import train_test_split
-from statsmodels.formula.api import ols
+#from statsmodels.formula.api import ols
+import statsmodels.api as sm
+#from statsmodels.stats.outliers_influence import variance_inflation_factor as vif
+
 
 
 os.chdir('C:/Users/samcl/Documents/GitHub/Seoul-Bike-Share')
 from modules.data import *
 from modules.plots import *
 
-filename = '../Seoul-Bike-Share/SeoulBikeData.csv'
-dv = 'Rented Bike Count'
-dummy_var = ('Hour','Seasons','Is Holiday')
-iv_col = ['Temperature', 'Humidity', 'Wind speed', 'Visibility', 'Dew point temperature', 'Solar Radiation', 'Rainfall', 'Snowfall' ]
+filename = '../Seoul-Bike-Share/SeoulBikeData.csv' #filename of dataset
+dv = 'Rented Bike Count' #dependent variable
+dummy_var = ('Hour','Seasons','Is Holiday') #list of dummy variables
+iv_col = ['Temperature', 'Humidity', 'Wind speed', 'Visibility', 'Dew point temperature', 'Solar Radiation', 'Rainfall', 'Snowfall' ] #independent variables/non-dummy variables
 drop_dummy = (23, 'No Holiday','Autumn') #select which dummy variables will be considered base
 
 
-df = data().getData(filename)
+df = data().getData(filename) #import data
 
-df.rename(columns={'Holiday':'Is Holiday'}, inplace = True)
+df.rename(columns={'Holiday':'Is Holiday'}, inplace = True) #rename holiday column for clarity
 
-df = data().renameCol(df)
+df = data().renameCol(df) #remove certain characters from headings
 
 
 #Create DF with descriptive statistics of IV
@@ -44,22 +47,16 @@ plots().histogram(np.array(df[dv]), 40, ['Bikes Rented', 'Frequency'])
 #SquareRoot transform to get better fit for dependent variable
 df['Sqrt Bikes Rented'] = data().transform(np.array(df[dv]), 'sqrt')
 
-dv = 'Sqrt Bikes Rented'
+dv = 'Sqrt Bikes Rented' #set new dv name
 
-plots().histogram(np.array(df[dv]), 40, [dv, 'Frequency'])
+plots().histogram(np.array(df[dv]), 40, [dv, 'Frequency']) #replot histogram with transformed dv
 
-
-
-#print scatterplots
+#print scatterplots to check linearity
 for col in iv_col:
     plots().scatterplot(df, col, dv)
     
-
-
 #create dummy variables
 df = data().dummy(df, dummy_var)
-
-
 
 #drop base dummy variable
 for col in drop_dummy:
@@ -72,20 +69,32 @@ plots().correlation(corr_df)
 
 df_1 = df.loc[:, ~df.columns.isin(['Date', 'Hour', 'Rented Bike Count', 'Seasons', 'Is Holiday', 'Functioning Day'])]
 
-#removes variables with multicollinearity
+#removes variables with VIF>10
 df_1 = df_1.loc[:, ~df_1.columns.isin([data().vif(df_1, dv)])]
 
 
-
-
 #create separate dfs for dv and iv
-df_1_x = df_1.loc[:, ~df_1.columns.isin([dv])]
-df_1_y = df_1[dv]
+df_1_X = df_1.loc[:, ~df_1.columns.isin([dv])] #features df
+df_1_Y = df_1[dv] #dv df
 
 #train full model
-reg = sm.OLS(df_1_y,df_1_x).fit()
+reg = sm.OLS(df_1_Y,df_1_X).fit()
 
-ol_model = ols('rating ~ points', data=df_1).fit()
+#remove influential points & outliers
+inf = reg.get_influence()
+inf.plot_influence()
+
+
+
+
+
+
+
+
+
+
+print(reg.rsquared_adj)
+
 # Train the classifier
 #reg.fit(df_1_x, df_1_y)
 
